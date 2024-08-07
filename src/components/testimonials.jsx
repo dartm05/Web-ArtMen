2fc685f6433787a7a6cf7c988f94343c4d9cbb19
Daniella Arteaga <darteag@bancolombia.com.co>
2024-08-07 18:48:48 -0500

    Add testimonial videos to section

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -9,23 +9,12 @@ export const Testimonials = (props) => {
           <div className="container video-container">
             <div className="testimonial-wrapper">
               <div className="testimonial-video">
-                <video
-                  src="https://66b3b0a24bc7580a0bc21022--drluisarteagavilla-video.netlify.app/"
-                  type="video/mp4"
-                  autoPlay
-                  muted
-                  loop
-                  preload="auto"
-                ></video>
-              </div>
-            </div>
-            <div className="testimonial-wrapper">
-              <div className="testimonial-video">
                 <iframe
+                  src="https://youtube.com/embed/Homqh-hB7kU"
+                  type="video/mp4"
                   width="360"
                   height="515"
-                  src="https://youtube.com/embed/7rBDXb05C9c"
-                  style={{border:0}}
+                  style={{ border: 0 }}
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                 ></iframe>
@@ -37,7 +26,19 @@ export const Testimonials = (props) => {
                   width="360"
                   height="515"
                   src="https://youtube.com/embed/7rBDXb05C9c"
-                  style={{border:0}}
+                  style={{ border: 0 }}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+            <div className="testimonial-wrapper">
+              <div className="testimonial-video">
+                <iframe
+                  width="360"
+                  height="515"
+                  src="https://youtube.com/embed/FJiblyuLcdc"
+                  style={{ border: 0 }}
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                 ></iframe>
