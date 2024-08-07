8329537b30645a8a61d3d9b5fb3574817b826aa2
Daniella Arteaga <darteag@bancolombia.com.co>
2024-08-07 13:23:44 -0500

    Remove deprecated attribute frameborder

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -25,7 +25,7 @@ export const Testimonials = (props) => {
                   width="360"
                   height="515"
                   src="https://youtube.com/embed/7rBDXb05C9c"
-                  frameBorder="0"
+                  style={{border:0}}
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                 ></iframe>
@@ -37,7 +37,7 @@ export const Testimonials = (props) => {
                   width="360"
                   height="515"
                   src="https://youtube.com/embed/7rBDXb05C9c"
-                  frameBorder="0"
+                  style={{border:0}}
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                 ></iframe>
