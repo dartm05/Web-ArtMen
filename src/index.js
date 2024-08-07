51bf8689d9da3f7287d277cd78c0acbf6a68f910
Daniella Arteaga <darteag@bancolombia.com.co>
2024-08-07 13:19:48 -0500

    Update createRoot, render deprecated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import "i18next";
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
