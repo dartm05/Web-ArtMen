fa88b277b6794ff42487bab46adeec5aacd2b197
Daniella Arteaga <darteag@bancolombia.com.co>
2024-08-07 10:13:13 -0500

    Remove testimonial texts

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -45,24 +45,6 @@ export const Testimonials = (props) => {
             </div>
           </div>
         </div>
-        <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  <div className="testimonial">
-                    <div className="testimonial-image">
-                      {" "}
-                      <img src={d.img} alt="" />{" "}
-                    </div>
-                    <div className="testimonial-content">
-                      <p>"{d.text}"</p>
-                      <div className="testimonial-meta"> - {d.name} </div>
-                    </div>
-                  </div>
-                </div>
-              ))
-            : "loading"}
-        </div>
       </div>
     </div>
   );
